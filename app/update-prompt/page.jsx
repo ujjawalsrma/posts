eabcb38c1acc65c630b5a49887bc7a51b97ea2d7
ujjawalsrma <76587457+ujjawalsrma@users.noreply.github.com>
2023-05-31 13:18:05 +0530

    Update page.jsx

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -7,10 +7,17 @@ import { useState } from "react";
 import { useSession } from "next-auth/react";
 
 const UpdatePrompt = () => {
-    const { data: session } = useSession();
-    const router = useRouter();
-
-  
+   
+    const { data: session , status } = useSession();
+  const router = useRouter();
+  if(status === "loading"){
+    return<></>
+  }
+  if (status === "unauthenticated")
+  {
+    
+    router.push("/");
+  }else{
 
     const searchParams = useSearchParams();
     const idofdoc = searchParams.get("id");
@@ -18,12 +25,15 @@ const UpdatePrompt = () => {
     const [submitting, setIsSubmitting] = useState(false);
 
     const a = localStorage.getItem('Name');
+
     const b = JSON.parse(a)
 
     const [post, setPost] = useState({ prompt: b.prompt, tag: b.tag });
 
     const updatePrompt = async (e) => {
         e.preventDefault();
+        post.prompt = post.prompt.replaceAll('"', "'");
+        post.tag = post.tag.replaceAll('"', "'");
         setIsSubmitting(true);
         try {
             let res = await fetch(`/api/prompt/${idofdoc}`, {
@@ -58,7 +68,7 @@ const UpdatePrompt = () => {
                
             />
         </>
-    )
+    )}
 }
 
 export default UpdatePrompt
